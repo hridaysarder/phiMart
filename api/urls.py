c946bd121db083c1c77124aa460713cfdef8fd03
hridaysarder <hridoyaug12@gmail.com>
2025-03-13 17:46:53 +0600

    Added CartItemSerializer

--- api/urls.py
+++ api/urls.py
@@ -1,17 +1,20 @@
-from django.urls import path,include
-from product.views import ProductViewSet,CategoryViewSet,ReviewViewSet
+from django.urls import path, include
+from product.views import ProductViewSet, CategoryViewSet, ReviewViewSet
 from rest_framework_nested import routers
+from order.views import CartViewSet
 
 router = routers.DefaultRouter()
-router.register('products',ProductViewSet,basename='products')
-router.register('categories',CategoryViewSet)
+router.register('products', ProductViewSet, basename='products')
+router.register('categories', CategoryViewSet)
+router.register('carts', CartViewSet, basename='carts')
 
-product_router = routers.NestedSimpleRouter(router,'products',lookup='product')
-product_router.register('reviews',ReviewViewSet,basename='product-review')
+product_router = routers.NestedSimpleRouter(
+    router, 'products', lookup='product')
+product_router.register('reviews', ReviewViewSet, basename='product-review')
 
 # urlpatterns = router.urls
 
 urlpatterns = [
-    path('',include(router.urls)),
-    path('',include(product_router.urls)),
+    path('', include(router.urls)),
+    path('', include(product_router.urls)),
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
+from order.serializers import CartSerializer
+from order.models import Cart
 
 # Create your views here.
+
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
