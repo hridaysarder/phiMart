93cecfb81a741366e6deef397aa6ec7538c1c310
hridaysarder <hridoyaug12@gmail.com>
2025-03-24 19:27:46 +0600

    Added cloudinary storage

--- product/serializers.py
+++ product/serializers.py
@@ -16,6 +16,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField()
     class Meta:
         model = ProductImage
         fields = ['id', 'image']
